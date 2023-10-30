33a36b440eb201aa728b2a1d9f96584d3ac48da9
Abhigyan Tripathi <abhigyantrips@gmail.com>
2023-10-30 13:58:45 +0530

    chore: init sign-on form

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: 'https://forms.office.com/r/uS1grDcAuW',
+				permanent: false,
+			}
+		]
+	}
+}
+
+module.exports = nextConfig
