c850e7531f4f78c11f14efdd4e48c30a96150440
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-02-21 21:20:56 +0530

    feat: add github label for blogs

--- components/mdx/github-label.tsx
+++ components/mdx/github-label.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 
-export function MdxGithubLabel() {
+interface GithubLabel {
+  name: string;
+  color: `#${string}`;
+}
+
+const labels: { [slug: string]: GithubLabel } = {
+  'type-code': {
+    name: '💻 type: code',
+    color: '#123456',
+  },
+};
+
+export function MdxGithubLabel({ slug }: { slug: string }) {
   return (
     <span className="inline-block select-none rounded-full border border-blue-800 bg-blue-100 bg-opacity-75 px-2 py-1 text-xs font-semibold leading-none text-blue-800">
-      💻 type: code
+      {labels[slug].name}
     </span>
   );
 }
