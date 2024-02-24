717d981d06b968f0e93ec0849d15e6e68c7cfae5
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-02-24 23:59:24 +0530

    feat: use yup validation for forms

--- next.config.js
+++ next.config.js
@@ -17,7 +17,6 @@ module.exports = withContentlayer({
         hostname: 'abhigyantrips.dev',
       },
     ],
-    domains: ['abhigyantrips.dev'],
     formats: ['image/avif', 'image/webp'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
